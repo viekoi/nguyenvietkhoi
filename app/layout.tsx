fdf84c2e89d26d2b0dca64e842b8bd6cde9bf22f
viekoi <97336479+viekoi@users.noreply.github.com>
2023-08-26 22:43:07 +0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
     <html lang="en">
       <body>
         <NextThemeProvider attribute="class" defaultTheme="system" enableSystem>
-          <div className="grid grid-cols-5 min-h-screen bg-gradient-to-b from-blue-600 to-[#fefefe] dark:bg-gradient-to-b dark:from-black dark:from-35% dark:to-[#626262] dark:text-white ">
+          <div className="grid grid-cols-5 min-h-screen bg-gradient-to-b from-[#6dd3e7] to-[#fefefe] dark:bg-gradient-to-b dark:from-black dark:from-35% dark:to-[#626262] dark:text-white ">
             <div className="hidden lg:block lg:col-span-1"/>
             {children}
             <div className="hidden lg:block lg:col-span-1"/>
